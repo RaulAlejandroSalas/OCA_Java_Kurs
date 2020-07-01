2e0834f6fcdb23aa0f725503d6d564324beba41f
RSAL182 <RSAL182@PC14151641.cc-college.gecs.loc>
2020-07-01 09:53:57 +0200

    [ADD] Adding boolean, byte and short declaration variables

--- Tag3_DatenTypen_Operatoren/src/variablen/DemoVariablen.java
+++ Tag3_DatenTypen_Operatoren/src/variablen/DemoVariablen.java
@@ -1,5 +1,77 @@
 package variablen;
 
+/**
+ * @author RSAL182
+ *
+ */
 public class DemoVariablen {
-
+ 
+	public static void main(String[] args) {
+		
+		/**
+		 * Boolean DateType
+		 * */
+		
+		boolean b1 = true;
+		Boolean b2 = true;
+		
+		//boolean b3 = True; // does not compile True is not keyword
+		//Boolean b4 = False; // does not compile False is not keyword
+	    boolean b5  = new Boolean("True"); //true
+	    Boolean b6 = new Boolean("fAlse"); //false
+	    Boolean b7 = new Boolean("abcd");  //false
+	    
+	    
+	    
+	    /**
+		 * Byte DateType
+		 * */
+		
+	    byte byte1 = 127; 
+	    Byte byte2 = 127;
+	    
+	    //byte byte3 = 128; //does not compile, value is treated as integer since it is out of scope 
+	    //Byte byte4 = 128; //does not compile, value is treated as integer since it is out of scope 
+	    
+	    
+	    /**
+		 * short DateType
+		 * */
+	    short short_1 = 10000; 
+	    Short short_2 = 8888;
+	    
+		
+	    
+	    
+	    
+	    
+	    /**
+		 * Char DateType
+		 * */
+		
+	    
+	    
+	    /**
+		 * Int DateType
+		 * */
+		
+	    
+	    
+	    /**
+		 * long DateType
+		 * */
+	    
+	    /**
+		 * double DateType
+		 * */
+	    
+	    
+	    /**
+		 * float DateType
+		 * */
+		
+		
+		
+	}
+	
 }
